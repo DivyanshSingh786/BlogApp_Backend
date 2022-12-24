04b26300a8140593cfe21b9072e516cda0ca9aac
Divyansh Singh <93871896+DivyanshSingh786@users.noreply.github.com>
2022-12-24 20:47:28 +0530

    Delete src directory

--- src/routes/order.js
+++
@@ -1,9 +0,0 @@
-const { requireSignin, userMiddleware } = require("../common-middleware");
-const { addOrder, getOrders, getOrder } = require("../controller/order");
-const router = require("express").Router();
-
-router.post("/addOrder", requireSignin, userMiddleware, addOrder);
-router.get("/getOrders", requireSignin, userMiddleware, getOrders);
-router.post("/getOrder", requireSignin, userMiddleware, getOrder);
-
-module.exports = router;
